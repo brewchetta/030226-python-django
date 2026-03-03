a54ea1a6f3361c563b8f3f5eb6b798fe7cf9a17f
brewchetta <chett.tiller@gmail.com>
2026-03-03 17:00:54 -0500

    updates to views

--- day-02/video_project/video_app/views.py
+++ day-02/video_project/video_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+# video list (in future sessions we'll use a database)
 video_list = [
     "44UGXFu-cQI?si=nbOTHgqxI0ydnixZ",
     "nGIg40xs9e4?si=bJE-BzV-GfP4t13o",
@@ -10,7 +11,8 @@ video_list = [
 # HOME #
 def home(request):
     context = {
-        "videos_list": video_list
+        "videos_list": video_list,
+        "no_videos": len( video_list ) == 0
     }
     return render(request, "video_app/home.html", context)
 
