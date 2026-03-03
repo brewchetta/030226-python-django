ee0e1da903dd05ad7dab5cf4a371cbe68668684e
brewchetta <chett.tiller@gmail.com>
2026-03-03 12:34:40 -0500

    youtube videos

--- day-02/video_project/video_app/views.py
+++ day-02/video_project/video_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+video_list = [
+    "44UGXFu-cQI?si=nbOTHgqxI0ydnixZ",
+    "nGIg40xs9e4?si=bJE-BzV-GfP4t13o",
+    "DLzxrzFCyOs?si=CUPc1I2BZaD5n4vp"
+]
+
+
 # HOME #
 def home(request):
-    return render(request, "video_app/home.html")
+    context = {
+        "videos_list": video_list
+    }
+    return render(request, "video_app/home.html", context)
+
 
 # ABOUT #
 def about(request):
