9e482ace2029872667830707bb7273af054494e1
brewchetta <chett.tiller@gmail.com>
2026-03-04 16:44:12 -0500

    movies with characters and genres

--- day-03/database_project/movies_app/models.py
+++ day-03/database_project/movies_app/models.py
@@ -1,21 +1,55 @@
 from django.db import models
+from datetime import datetime
+
+# any time I make a change inside of models.py
+# python manage.py makemigrations
+# python manage.py migrate
 
 # MVC - model view controller
 
 # MODEL --> is a way to interface with the database --> talk to the db
 
+
+class Genre(models.Model):
+    name = models.CharField()
+    
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Movie(models.Model):
     # each movie also gets an id
-    title = models.CharField() # charfield is for strings
-    release_year = models.IntegerField(default=2026)
+    title = models.CharField() # short strings
+    release_year = models.IntegerField( default=2026 )
     run_time = models.IntegerField()
-    genre = models.CharField()
     description = models.TextField() # longer strings
-    rating = models.IntegerField( null=True )
+    rating = models.IntegerField( null=True, blank=True )
+    
+    # max_digits means we can't go higher than 999.99
+    # decimal_places means there will always be two decimals (0.01)
+    rental_price = models.DecimalField( max_digits=5, decimal_places=2, default=1.95 )
+    
+    genres = models.ManyToManyField(Genre, blank=True)
+    
+    # auto_now_add triggers whenever something gets created
+    created_at = models.DateTimeField( auto_now_add=True )
+    # auto_now triggers whenever something gets updated
+    updated_at = models.DateTimeField( auto_now=True )
     
     # shows the movie as just a string (in this case show the title of the movie)
     def __str__(self):
-        return f"{self.title} - {self.genre}"
+        return f"{self.title}"
+
+
+class MovieCharacter(models.Model):
+    name = models.CharField()
+    special_power = models.CharField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="characters")
+                    # 1. the model we're associating with
+                    # 2. what happens when we delete a movie (delete all movie characters)
+                    #  3. what we can call on a movie to see all its characters
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
 
 
 # INHERITANCE #
--- day-03/database_project/movies_app/views.py
+++ day-03/database_project/movies_app/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
+# HOME (OLD VERSION) #
+# def home(request):
+#     # get all movies from the db
+#     all_movies = Movie.objects.all()
+#     # put the movies into context
+#     context = { "all_movies": all_movies }
+#     # render page with context
+#     return render(request, "movies_app/home.html", context)
+
 # HOME #
 def home(request):
-    # get all movies from the db
-    all_movies = Movie.objects.all()
-    # put the movies into context
-    context = { "all_movies": all_movies }
-    # render page with context
+    movies_query_set = Movie.objects
+
+    # get movies for 2026
+    # movies_query_set = movies_query_set.filter( release_year=2026 )
+
+    # gets movies before 2026
+    # movies_query_set = movies_query_set.filter( release_year__lt=2026 )
+
+    # gets movies with the word `predator` in their title
+    # movies_query_set = movies_query_set.filter( title__icontains="predator" )
+
+    # http://127.0.0.1:8000/?title=predator&release_year=2026
+    # title=predator
+    query_title = request.GET.get('title')
+    # release_year=2026
+    query_release_year = request.GET.get('release_year')
+    
+    # if you have the query_title then filter
+    if query_title:
+        movies_query_set = movies_query_set.filter(title__icontains=query_title)
+    # if you have the query_release_year then filter
+    if query_release_year:
+        movies_query_set = movies_query_set.filter(release_year=query_release_year)
+
+    context = { "all_movies": movies_query_set.all() }
+
     return render(request, "movies_app/home.html", context)
 
 # MOVIE DETAIL #
